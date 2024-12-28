e0f193378b4ede09a743658dc66892e5c9b99976
Taf Makura <tafmakura@gmail.com>
2024-12-29 01:47:03 +0200

    test

--- includes/classes/class-custom-post-types.php
+++ includes/classes/class-custom-post-types.php
@@ -5,7 +5,7 @@ namespace Siya;
 class CustomPostTypes {
     public function __construct() {
         add_action('init', array($this,'create_post_type_called_server'));
-        echo "44444444444444444444444444444444444444444 44444444444d88833";
+        echo "44444444444444444444444444444444444444444 44444444444d98833";
     }
 
     public function create_post_type_called_server() {
--- siya-server-manager.php
+++ siya-server-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SIYA Server Manager
  * Description: Server integration and yield augmentation plugin for WooCommerce.
- * Version: 0.0.3
+ * Version: 0.0.4
  * Author: Tafadzwa Makura
  * Text Domain: arsol
  */
