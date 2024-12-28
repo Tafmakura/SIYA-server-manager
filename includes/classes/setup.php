cc8d6e62284c3afb15a86af86adaa451c610a58e
Taf Makura <tafmakura@gmail.com>
2024-12-28 20:41:46 +0200

    Commit

--- includes/classes/setup.php
+++ includes/classes/setup.php
@@ -3,8 +3,17 @@
 namespace Siya;
 
 class Setup {
+    public function __construct() {
+        $this->include_files();
+        $this->initialize_hooks();
+    }
 
+    private function include_files() {
+        require_once plugin_dir_path(__FILE__) . '/../admin-functions.php';
+        require_once plugin_dir_path(__FILE__) . '/../logic-functions.php';
+    }
 
+    private function initialize_hooks() {
+        // Add any initialization hooks here
+    }
 }
-
-$siyaServerManager = new Setup();
