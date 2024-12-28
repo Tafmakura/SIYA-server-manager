74f75d6713aa3dc8a0ee272456261e2e6ebda4ce
Taf Makura <tafmakura@gmail.com>
2024-12-29 01:41:26 +0200

    Update class-custom-post-types.php

--- includes/classes/class-custom-post-types.php
+++ includes/classes/class-custom-post-types.php
@@ -5,9 +5,7 @@ namespace Siya;
 class CustomPostTypes {
     public function __construct() {
         add_action('init', array($this, 'create_server_post_type'));
-
-        echo "44444444444444444444444444444444444444444 44444444444d443333";
-
+        echo "44444444444444444444444444444444444444444 44444444444d33333";
     }
 
     public function create_server_post_type() {
