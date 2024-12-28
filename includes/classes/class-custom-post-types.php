f94d13594b8793ded859075a56145adedf7023e5
Taf Makura <tafmakura@gmail.com>
2024-12-29 01:53:49 +0200

    Update class-custom-post-types.php

--- includes/classes/class-custom-post-types.php
+++ includes/classes/class-custom-post-types.php
@@ -4,62 +4,10 @@ namespace Siya;
 
 class CustomPostTypes {
     public function __construct() {
-        add_action('init', array($this,'create_post_type_called_server'));
-        echo "44444444444444444444444444444444444444444 44444444444d98833";
-
-
-        $labels = array(
-            'name'               => _x('Servers', 'post type general name', 'your-text-domain'),
-            'singular_name'      => _x('Server', 'post type singular name', 'your-text-domain'),
-            'menu_name'          => _x('Servers', 'admin menu', 'your-text-domain'),
-            'add_new'            => _x('Add New', 'server', 'your-text-domain'),
-            'add_new_item'       => __('Add New Server', 'your-text-domain'),
-            'edit_item'          => __('Edit Server', 'your-text-domain'),
-            'view_item'          => __('View Server', 'your-text-domain'),
-            'all_items'          => __('All Servers', 'your-text-domain'),
-            'search_items'       => __('Search Servers', 'your-text-domain'),
-            'not_found'          => __('No servers found.', 'your-text-domain'),
-            'not_found_in_trash' => __('No servers found in Trash.', 'your-text-domain')
-        );
-
-        $args = array(
-            'labels'             => $labels,
-            'public'             => true,
-            'show_ui'            => true,
-            'show_in_menu'       => true,
-            'query_var'          => true,
-            'rewrite'            => array('slug' => 'server', 'with_front' => false),
-            'capability_type'    => 'post',
-            'has_archive'        => true,
-            'hierarchical'       => false,
-            'menu_position'      => null,
-            'supports'           => array(
-                'title',
-                'editor',
-                'author',
-                'thumbnail',
-                'excerpt',
-                'comments'
-            ),
-        );
-
-        register_post_type('server', $args);
-
-
-
-
-
-
-
-
-
-
+        $this->create_server_post_type();
     }
 
-    public function create_post_type_called_server() {
-
-        echo "44444444444444444444444444444444444444444 44444444444d44777";
-
+    public function create_server_post_type() {
         $labels = array(
             'name'               => _x('Servers', 'post type general name', 'your-text-domain'),
             'singular_name'      => _x('Server', 'post type singular name', 'your-text-domain'),
@@ -85,17 +33,9 @@ class CustomPostTypes {
             'has_archive'        => true,
             'hierarchical'       => false,
             'menu_position'      => null,
-            'supports'           => array(
-                'title',
-                'editor',
-                'author',
-                'thumbnail',
-                'excerpt',
-                'comments'
-            ),
+            'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
         );
 
         register_post_type('server', $args);
-        flush_rewrite_rules(); // Only use this temporarily
     }
 }
