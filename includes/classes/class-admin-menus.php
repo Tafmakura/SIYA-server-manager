554f02822f388aee6981ece869f582ed2f3cbfd5
Taf Makura <tafmakura@gmail.com>
2024-12-29 03:00:12 +0200

    OOP

--- includes/classes/class-admin-menus.php
+++ includes/classes/class-admin-menus.php
@@ -4,18 +4,29 @@ namespace Siya;
 
 class AdminMenus {
     public function __construct() {
-        $this->runcloud_settings_menu();
+        $this->add_siya_menu();
     }
 
     /**
-     * Add settings menu to the WordPress admin.
+     * Add top-level SIYA menu and API Settings submenu to the WordPress admin.
      */
-    public function runcloud_settings_menu() {
-        add_options_page(
-            'RunCloud and Hetzner Settings',  // Page title
-            'API Settings',                   // Menu title
-            'manage_options',                 // Capability
-            'api-settings',                   // Menu slug
+    public function add_siya_menu() {
+        add_menu_page(
+            'SIYA',                            // Page title
+            'SIYA',                            // Menu title
+            'manage_options',                  // Capability
+            'siya-menu',                       // Menu slug
+            '',                                // Callback function (empty for top-level menu)
+            'dashicons-admin-generic',         // Icon URL
+            6                                  // Position
+        );
+
+        add_submenu_page(
+            'siya-menu',                       // Parent slug
+            'RunCloud and Hetzner Settings',   // Page title
+            'API Settings',                    // Menu title
+            'manage_options',                  // Capability
+            'api-settings',                    // Menu slug
             array('Siya\AdminSettings', 'runcloud_settings_page') // Callback function
         );
     }
