a0adc60fc1fef22ee92ee1d9d938abd905c7b4d7
Taf Makura <tafmakura@gmail.com>
2024-12-29 03:58:29 +0200

    Update siya-server-manager.php

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SIYA Server Manager
  * Description: Server integration and yield augmentation plugin for WooCommerce.
- * Version: 0.0.10
+ * Version: 0.0.11
  * Author: Tafadzwa Makura
  * Text Domain: arsol
  */
