e34cd5d68c30b653632bb1cfb7b3b530a18782b2
Taf Makura <tafmakura@gmail.com>
2024-12-31 00:24:19 +0200

    Comiit

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -16,32 +16,27 @@ class Setup {
         // Core Classes
         require_once plugin_dir_path(__DIR__) . '/classes/class-setup.php';
         
-        // Custom Post Types
-        require_once plugin_dir_path(__DIR__) . '/custom-post-types/server/class-server-cpt.php';
-
-        // Admin
-        require_once plugin_dir_path(__DIR__) . '/classes/class-admin-settings.php';
-        require_once plugin_dir_path(__DIR__) . '/classes/class-admin-menus.php';
-        
         // Interfaces
-        require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-provider.php';
+        
+        //require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-provider.php';
         require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-provider-setup.php';
-        require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-manager.php';
+        //require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-manager.php';
         require_once plugin_dir_path(__DIR__) . '/interfaces/interface-server-manager-setup.php';
 
-        // Server Providers
-        require_once plugin_dir_path(__DIR__) . '../integrations/server-providers/hetzner/class-hetzner.php';
-        require_once plugin_dir_path(__DIR__) . '../integrations/server-providers/hetzner/class-hetzner-setup.php';
+        // Custom Post Types
+        require_once plugin_dir_path(__DIR__) . '/custom-post-types/server/class-server-cpt.php';
         
-        // Server Managers
-        require_once plugin_dir_path(__DIR__) . '../integrations/server-managers/runcloud/class-runcloud.php';
-        require_once plugin_dir_path(__DIR__) . '../integrations/server-managers/runcloud/class-runcloud-setup.php';
+        // Integrations
+        require_once plugin_dir_path(__DIR__) . '/integrations/server-providers/hetzner/class-hetzner.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/server-providers/hetzner/class-hetzner-setup.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/server-managers/runcloud/class-runcloud.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/server-managers/runcloud/class-runcloud-setup.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-orchestrator.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-circuit-breaker.php';
 
-        // WooCommerce Subscriptions
-        require_once plugin_dir_path(__DIR__) . '/classes/class-server-orchestrator.php';
-        require_once plugin_dir_path(__DIR__) . '/classes/class-server-circuit-breaker.php';
-
-
+        // Admin
+        require_once plugin_dir_path(__DIR__) . '/admin/class-admin-settings.php';
+        require_once plugin_dir_path(__DIR__) . '/admin/class-admin-menus.php';
     }
 
     /**
