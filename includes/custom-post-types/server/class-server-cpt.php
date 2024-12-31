c28cb5557823887a0470cb8bddb33050dd40c5db
Taf Makura <tafmakura@gmail.com>
2024-12-31 14:14:22 +0200

    f

--- includes/custom-post-types/server/class-server-cpt.php
+++ includes/custom-post-types/server/class-server-cpt.php
@@ -3,7 +3,6 @@
 namespace SIYA\CustomPostTypes;
 
 class ServerPost {
-    const META_PREFIX = 'arsol_server_';
     protected $post_id;
   
     public $server_post_name;
@@ -40,30 +39,30 @@ class ServerPost {
     }
 
     private function load_meta_data() {
-        $this->server_post_name = get_post_meta($this->post_id, self::META_PREFIX . 'post_name', true);
-        $this->server_deployed_id = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_id', true);
-        $this->server_provisioned_id = get_post_meta($this->post_id, self::META_PREFIX . 'provisioned_id', true);
-        $this->server_subscription_id = get_post_meta($this->post_id,  self::META_PREFIX .'subscription_id', true);
-        $this->server_max_applications = get_post_meta($this->post_id, self::META_PREFIX . 'max_applications', true);
-        $this->server_deployed_applications = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_applications', true);
-        $this->server_max_staging_sites = get_post_meta($this->post_id, self::META_PREFIX . 'max_staging_sites', true);
-        $this->server_deployed_staging_sites = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_staging_sites', true);
-        $this->server_type = get_post_meta($this->post_id, self::META_PREFIX . 'type', true);
-        $this->server_provisioned_os = get_post_meta($this->post_id, self::META_PREFIX . 'os', true);
-        $this->server_provisioned_os_version = get_post_meta($this->post_id, self::META_PREFIX . 'os_version', true);
-        $this->server_provisioned_region = get_post_meta($this->post_id, self::META_PREFIX . 'region', true);
-        $this->server_deployment_manager = get_post_meta($this->post_id, self::META_PREFIX . 'manager', true);
-        $this->server_deployed_name = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_name', true);
-        $this->server_deployed_status = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_status', true);
-        $this->server_deployed_date = get_post_meta($this->post_id, self::META_PREFIX . 'deployed_date', true);
-        $this->server_provisioning_provider = get_post_meta($this->post_id, self::META_PREFIX . 'provider', true);
-        $this->server_provisioned_name = get_post_meta($this->post_id, self::META_PREFIX . 'provisioned_name', true);
-        $this->server_provisioned_status = get_post_meta($this->post_id, self::META_PREFIX . 'provisioned_status', true);
-        $this->server_provisioned_date = get_post_meta($this->post_id, self::META_PREFIX . 'provisioned_date', true);
-        $this->server_status_date = get_post_meta($this->post_id, self::META_PREFIX . 'status_date', true);
-        $this->server_provisioned_ipv4 = get_post_meta($this->post_id, self::META_PREFIX . 'ipv4', true);
-        $this->server_provisioned_ipv6 = get_post_meta($this->post_id, self::META_PREFIX . 'ipv6', true);
-        $this->server_provisioned_root_password = get_post_meta($this->post_id, self::META_PREFIX . 'root_password', true);
+        $this->server_post_name = get_post_meta($this->post_id, 'arsol_server_post_name', true);
+        $this->server_deployed_id = get_post_meta($this->post_id, 'arsol_server_deployed_id', true);
+        $this->server_provisioned_id = get_post_meta($this->post_id, 'arsol_server_provisioned_id', true);
+        $this->server_subscription_id = get_post_meta($this->post_id,  'arsol_server_subscription_id', true);
+        $this->server_max_applications = get_post_meta($this->post_id, 'arsol_server_max_applications', true);
+        $this->server_deployed_applications = get_post_meta($this->post_id, 'arsol_server_deployed_applications', true);
+        $this->server_max_staging_sites = get_post_meta($this->post_id, 'arsol_server_max_staging_sites', true);
+        $this->server_deployed_staging_sites = get_post_meta($this->post_id, 'arsol_server_deployed_staging_sites', true);
+        $this->server_type = get_post_meta($this->post_id, 'arsol_server_type', true);
+        $this->server_provisioned_os = get_post_meta($this->post_id, 'arsol_server_os', true);
+        $this->server_provisioned_os_version = get_post_meta($this->post_id, 'arsol_server_os_version', true);
+        $this->server_provisioned_region = get_post_meta($this->post_id, 'arsol_server_region', true);
+        $this->server_deployment_manager = get_post_meta($this->post_id, 'arsol_server_manager', true);
+        $this->server_deployed_name = get_post_meta($this->post_id, 'arsol_server_deployed_name', true);
+        $this->server_deployed_status = get_post_meta($this->post_id, 'arsol_server_deployed_status', true);
+        $this->server_deployed_date = get_post_meta($this->post_id, 'arsol_server_deployed_date', true);
+        $this->server_provisioning_provider = get_post_meta($this->post_id, 'arsol_server_provider', true);
+        $this->server_provisioned_name = get_post_meta($this->post_id, 'arsol_server_provisioned_name', true);
+        $this->server_provisioned_status = get_post_meta($this->post_id, 'arsol_server_provisioned_status', true);
+        $this->server_provisioned_date = get_post_meta($this->post_id, 'arsol_server_provisioned_date', true);
+        $this->server_status_date = get_post_meta($this->post_id, 'arsol_server_status_date', true);
+        $this->server_provisioned_ipv4 = get_post_meta($this->post_id, 'arsol_server_ipv4', true);
+        $this->server_provisioned_ipv6 = get_post_meta($this->post_id, 'arsol_server_ipv6', true);
+        $this->server_provisioned_root_password = get_post_meta($this->post_id, 'arsol_server_root_password', true);
     }
     
     public function create_server_post($subscription_id) {
@@ -80,7 +79,7 @@ class ServerPost {
             'post_type' => 'server',
             'meta_query' => array(
                 array(
-                    'key' => self::META_PREFIX . 'subscription_id',
+                    'key' => 'arsol_server_subscription_id',
                     'value' => $subscription_id
                 )
             )
@@ -103,7 +102,7 @@ class ServerPost {
             'post_type' => 'server',
             'meta_query' => array(
                 array(
-                    'key' => self::META_PREFIX . 'subscription_id',
+                    'key' => 'arsol_server_subscription_id',
                     'value' => $subscription_id
                 )
             )
@@ -148,7 +147,7 @@ class ServerPost {
         $data = wp_parse_args($meta_data, $defaults);
         
         foreach ($data as $key => $value) {
-            update_post_meta($post_id, self::META_PREFIX . $key, sanitize_text_field($value));
+            update_post_meta($post_id, 'arsol_server_' . $key, sanitize_text_field($value));
         }
         
         return true;
@@ -158,32 +157,32 @@ class ServerPost {
         $this->post_id = $post_id;
         
         $meta_data = array(
-            'provisioned_id' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_id', true),
-            'provisioned_os' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_os', true),
-            'provisioned_os_version' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_os_version', true),
-            'provisioned_region' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_region', true),
-            'provisioning_provider' => get_post_meta($post_id, self::META_PREFIX . 'provisioning_provider', true),
-            'provisioned_name' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_name', true),
-            'provisioned_status' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_status', true),
-            'provisioned_date' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_date', true),
-            'provisioned_ipv4' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_ipv4', true),
-            'provisioned_ipv6' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_ipv6', true),
-            'provisioned_root_password' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_root_password', true),
-            'provisioned_applications' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_applications', true),
-            'provisioned_staging_sites' => get_post_meta($post_id, self::META_PREFIX . 'provisioned_staging_sites', true),
-            'deployed_id' => get_post_meta($post_id, self::META_PREFIX . 'deployed_id', true),
-            'deployed_applications' => get_post_meta($post_id, self::META_PREFIX . 'deployed_applications', true),
-            'deployed_staging_sites' => get_post_meta($post_id, self::META_PREFIX . 'deployed_staging_sites', true),
-            'deployment_manager' => get_post_meta($post_id, self::META_PREFIX . 'deployment_manager', true),
-            'deployed_name' => get_post_meta($post_id, self::META_PREFIX . 'deployed_name', true),
-            'deployed_status' => get_post_meta($post_id, self::META_PREFIX . 'deployed_status', true),
-            'deployed_date' => get_post_meta($post_id, self::META_PREFIX . 'deployed_date', true),
-            'subscription_id' => get_post_meta($post_id, self::META_PREFIX . 'subscription_id', true),
-            'max_applications' => get_post_meta($post_id, self::META_PREFIX . 'max_applications', true),
-            'max_staging_sites' => get_post_meta($post_id, self::META_PREFIX . 'max_staging_sites', true),
-            'type' => get_post_meta($post_id, self::META_PREFIX . 'type', true),
-            'manager' => get_post_meta($post_id, self::META_PREFIX . 'manager', true),
-            'status_date' => get_post_meta($post_id, self::META_PREFIX . 'status_date', true),
+            'provisioned_id' => get_post_meta($post_id, 'arsol_server_provisioned_id', true),
+            'provisioned_os' => get_post_meta($post_id, 'arsol_server_provisioned_os', true),
+            'provisioned_os_version' => get_post_meta($post_id, 'arsol_server_provisioned_os_version', true),
+            'provisioned_region' => get_post_meta($post_id, 'arsol_server_provisioned_region', true),
+            'provisioning_provider' => get_post_meta($post_id, 'arsol_server_provisioning_provider', true),
+            'provisioned_name' => get_post_meta($post_id, 'arsol_server_provisioned_name', true),
+            'provisioned_status' => get_post_meta($post_id, 'arsol_server_provisioned_status', true),
+            'provisioned_date' => get_post_meta($post_id, 'arsol_server_provisioned_date', true),
+            'provisioned_ipv4' => get_post_meta($post_id, 'arsol_server_provisioned_ipv4', true),
+            'provisioned_ipv6' => get_post_meta($post_id, 'arsol_server_provisioned_ipv6', true),
+            'provisioned_root_password' => get_post_meta($post_id, 'arsol_server_provisioned_root_password', true),
+            'provisioned_applications' => get_post_meta($post_id, 'arsol_server_provisioned_applications', true),
+            'provisioned_staging_sites' => get_post_meta($post_id, 'arsol_server_provisioned_staging_sites', true),
+            'deployed_id' => get_post_meta($post_id, 'arsol_server_deployed_id', true),
+            'deployed_applications' => get_post_meta($post_id, 'arsol_server_deployed_applications', true),
+            'deployed_staging_sites' => get_post_meta($post_id, 'arsol_server_deployed_staging_sites', true),
+            'deployment_manager' => get_post_meta($post_id, 'arsol_server_deployment_manager', true),
+            'deployed_name' => get_post_meta($post_id, 'arsol_server_deployed_name', true),
+            'deployed_status' => get_post_meta($post_id, 'arsol_server_deployed_status', true),
+            'deployed_date' => get_post_meta($post_id, 'arsol_server_deployed_date', true),
+            'subscription_id' => get_post_meta($post_id, 'arsol_server_subscription_id', true),
+            'max_applications' => get_post_meta($post_id, 'arsol_server_max_applications', true),
+            'max_staging_sites' => get_post_meta($post_id, 'arsol_server_max_staging_sites', true),
+            'type' => get_post_meta($post_id, 'arsol_server_type', true),
+            'manager' => get_post_meta($post_id, 'arsol_server_manager', true),
+            'status_date' => get_post_meta($post_id, 'arsol_server_status_date', true),
     );
         
         return array_filter($meta_data);
