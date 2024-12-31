28bbbd31a821f8b48ddb3418f724c69b91dac24a
Taf Makura <tafmakura@gmail.com>
2024-12-31 16:47:40 +0200

    erew

--- includes/custom-post-types/server/class-server-cpt.php
+++ includes/custom-post-types/server/class-server-cpt.php
@@ -3,7 +3,7 @@
 namespace SIYA\CustomPostTypes;
 
 class ServerPost {
-    protected $post_id;
+    private $post_id;
   
     public $server_post_name;
     public $server_post_creation_date;
@@ -155,33 +155,33 @@ class ServerPost {
         $this->post_id = $post_id;
         
         $meta_data = array(
-            'provisioned_id' => get_post_meta($post_id, 'arsol_server_provisioned_id', true),
-            'provisioned_os' => get_post_meta($post_id, 'arsol_server_provisioned_os', true),
-            'provisioned_os_version' => get_post_meta($post_id, 'arsol_server_provisioned_os_version', true),
-            'provisioned_region' => get_post_meta($post_id, 'arsol_server_provisioned_region', true),
-            'provisioning_provider' => get_post_meta($post_id, 'arsol_server_provisioning_provider', true),
-            'provisioned_name' => get_post_meta($post_id, 'arsol_server_provisioned_name', true),
-            'provisioned_status' => get_post_meta($post_id, 'arsol_server_provisioned_status', true),
-            'provisioned_date' => get_post_meta($post_id, 'arsol_server_provisioned_date', true),
-            'provisioned_ipv4' => get_post_meta($post_id, 'arsol_server_provisioned_ipv4', true),
-            'provisioned_ipv6' => get_post_meta($post_id, 'arsol_server_provisioned_ipv6', true),
-            'provisioned_root_password' => get_post_meta($post_id, 'arsol_server_provisioned_root_password', true),
-            'provisioned_applications' => get_post_meta($post_id, 'arsol_server_provisioned_applications', true),
-            'provisioned_staging_sites' => get_post_meta($post_id, 'arsol_server_provisioned_staging_sites', true),
-            'deployed_id' => get_post_meta($post_id, 'arsol_server_deployed_id', true),
-            'deployed_applications' => get_post_meta($post_id, 'arsol_server_deployed_applications', true),
-            'deployed_staging_sites' => get_post_meta($post_id, 'arsol_server_deployed_staging_sites', true),
-            'deployment_manager' => get_post_meta($post_id, 'arsol_server_deployment_manager', true),
-            'deployed_name' => get_post_meta($post_id, 'arsol_server_deployed_name', true),
-            'deployed_status' => get_post_meta($post_id, 'arsol_server_deployed_status', true),
-            'deployed_date' => get_post_meta($post_id, 'arsol_server_deployed_date', true),
-            'subscription_id' => get_post_meta($post_id, 'arsol_server_subscription_id', true),
-            'max_applications' => get_post_meta($post_id, 'arsol_server_max_applications', true),
-            'max_staging_sites' => get_post_meta($post_id, 'arsol_server_max_staging_sites', true),
-            'type' => get_post_meta($post_id, 'arsol_server_type', true),
-            'manager' => get_post_meta($post_id, 'arsol_server_manager', true),
-            'status_date' => get_post_meta($post_id, 'arsol_server_status_date', true),
-    );
+            'arsol_server_provisioned_id' => get_post_meta($post_id, 'arsol_server_provisioned_id', true),
+            'arsol_server_provisioned_os' => get_post_meta($post_id, 'arsol_server_provisioned_os', true),
+            'arsol_server_provisioned_os_version' => get_post_meta($post_id, 'arsol_server_provisioned_os_version', true),
+            'arsol_server_provisioned_region' => get_post_meta($post_id, 'arsol_server_provisioned_region', true),
+            'arsol_server_provisioning_provider' => get_post_meta($post_id, 'arsol_server_provisioning_provider', true),
+            'arsol_server_provisioned_name' => get_post_meta($post_id, 'arsol_server_provisioned_name', true),
+            'arsol_server_provisioned_status' => get_post_meta($post_id, 'arsol_server_provisioned_status', true),
+            'arsol_server_provisioned_date' => get_post_meta($post_id, 'arsol_server_provisioned_date', true),
+            'arsol_server_provisioned_ipv4' => get_post_meta($post_id, 'arsol_server_provisioned_ipv4', true),
+            'arsol_server_provisioned_ipv6' => get_post_meta($post_id, 'arsol_server_provisioned_ipv6', true),
+            'arsol_server_provisioned_root_password' => get_post_meta($post_id, 'arsol_server_provisioned_root_password', true),
+            'arsol_server_provisioned_applications' => get_post_meta($post_id, 'arsol_server_provisioned_applications', true),
+            'arsol_server_provisioned_staging_sites' => get_post_meta($post_id, 'arsol_server_provisioned_staging_sites', true),
+            'arsol_server_deployed_id' => get_post_meta($post_id, 'arsol_server_deployed_id', true),
+            'arsol_server_deployed_applications' => get_post_meta($post_id, 'arsol_server_deployed_applications', true),
+            'arsol_server_deployed_staging_sites' => get_post_meta($post_id, 'arsol_server_deployed_staging_sites', true),
+            'arsol_server_deployment_manager' => get_post_meta($post_id, 'arsol_server_deployment_manager', true),
+            'arsol_server_deployed_name' => get_post_meta($post_id, 'arsol_server_deployed_name', true),
+            'arsol_server_deployed_status' => get_post_meta($post_id, 'arsol_server_deployed_status', true),
+            'arsol_server_deployed_date' => get_post_meta($post_id, 'arsol_server_deployed_date', true),
+            'arsol_server_subscription_id' => get_post_meta($post_id, 'arsol_server_subscription_id', true),
+            'arsol_server_max_applications' => get_post_meta($post_id, 'arsol_server_max_applications', true),
+            'arsol_server_max_staging_sites' => get_post_meta($post_id, 'arsol_server_max_staging_sites', true),
+            'arsol_server_type' => get_post_meta($post_id, 'arsol_server_type', true),
+            'arsol_server_manager' => get_post_meta($post_id, 'arsol_server_manager', true),
+            'arsol_server_status_date' => get_post_meta($post_id, 'arsol_server_status_date', true),
+        );
         
         return array_filter($meta_data);
     }
