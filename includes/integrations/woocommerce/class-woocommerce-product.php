ba900886607b355a8656e20c9261e8429be5e788
Taf Makura <tafmakura@gmail.com>
2025-01-02 18:12:50 +0200

    Update class-woocommerce-product.php

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -5,7 +5,7 @@ namespace Siya\Integrations\WooCommerce;
 defined('ABSPATH') || exit;
 
 class Product {
-   
+
     public function __construct() {
         add_action('init', [$this, 'init']);
     }
@@ -16,31 +16,31 @@ class Product {
         }
 
         // Add custom product type options
-        add_filter('product_type_options', [$this, 'add_e_visa_product_option']);
-        add_action( 'woocommerce_process_product_meta_simple', [$this, 'save_evisa_option_fields']  );
-        add_action( 'woocommerce_process_product_meta_variable', [$this, 'save_evisa_option_fields']  );
+        add_filter('product_type_options', [$this, 'add_server_product_option']);
+        add_action('woocommerce_process_product_meta_simple', [$this, 'save_server_option_fields']);
+        add_action('woocommerce_process_product_meta_variable', [$this, 'save_server_option_fields']);
     }
 
     public function get_product($product_id) {
         return wc_get_product($product_id);
     }
 
-    public function add_e_visa_product_option($product_type_options) {
+    public function add_server_product_option($product_type_options) {
         // Add your custom product type options here
-        $product_type_options['evisa'] = array(
+        $product_type_options['_arsol_server'] = array(
             'id'            => '_arsol_server',
             'wrapper_class' => 'show_if_simple show_if_variable',
-            'label'         => __( 'Server', 'woocommerce' ),
-            'description'   => __( '', 'woocommerce' ),
+            'label'         => __('Server', 'woocommerce'),
+            'description'   => __('', 'woocommerce'),
             'default'       => 'no'
         );
-    
-        return $product_type_options;
 
+        return $product_type_options;
     }
 
-    public function save_evisa_option_fields( $post_id ) {
-        $is_e_visa = isset( $_POST['_arsol_server'] ) ? 'yes' : 'no';
-        update_post_meta( $post_id, '_arsol_server', $is_e_visa );
+    public function save_server_option_fields($post_id) {
+        // Sanitize and validate the input
+        $is_server = isset($_POST['_arsol_server']) ? 'yes' : 'no';
+        update_post_meta($post_id, '_arsol_server', sanitize_text_field($is_server));
     }
 }
