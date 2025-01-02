1042d46e788d57b3844d0dca93220e4d2b1973ab
Taf Makura <tafmakura@gmail.com>
2025-01-02 18:54:20 +0200

    Update class-woocommerce-product.php

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -24,7 +24,7 @@ class Product {
     public function add_arsol_server_product_option($product_type_options) {
         // Add your custom product type options here
         $product_type_options['arsol_server'] = array(
-            'id'            => '_arsol_server',
+            'id'            => '_is_arsol_server',
             'wrapper_class' => 'show_if_simple show_if_variable',
             'label'         => __('Arsol Server', 'woocommerce'),
             'description'   => __('', 'woocommerce'),
@@ -36,7 +36,7 @@ class Product {
 
     public function save_arsol_server_option_fields($post_id) {
         $is_arsol_server = isset($_POST['_arsol_server']) ? 'yes' : 'no';
-        update_post_meta($post_id, '_arsol_server', $is_arsol_server);
+        update_post_meta($post_id, '_is_arsol_server', $is_arsol_server);
     }
 }
 
