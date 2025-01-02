74ae9d8fc77636f75fadfbf8fe5c67a9eb303393
Taf Makura <tafmakura@gmail.com>
2025-01-02 20:03:00 +0200

    Update class-woocommerce-product.php

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -19,6 +19,10 @@ class Product {
         add_filter('product_type_options', [$this, 'add_arsol_server_product_option']);
         add_action('woocommerce_process_product_meta_simple', [$this, 'save_arsol_server_option_fields']);
         add_action('woocommerce_process_product_meta_variable', [$this, 'save_arsol_server_option_fields']);
+        
+        // Add custom tab
+        add_filter('woocommerce_product_tabs', [$this, 'add_custom_product_tab']);
+        add_action('woocommerce_product_tab_panels', [$this, 'display_custom_product_tab_panel']);
     }
 
     public function add_arsol_server_product_option($product_type_options) {
@@ -38,8 +42,21 @@ class Product {
         $is_arsol_server = isset($_POST['_arsol_server']) ? 'yes' : 'no';
         update_post_meta($post_id, '_arsol_server', $is_arsol_server);
     }
+
+    public function add_custom_product_tab($tabs) {
+        $tabs['custom_tab'] = array(
+            'title'    => __('Custom Tab', 'woocommerce'),
+            'priority' => 50,
+            'callback' => [$this, 'display_custom_product_tab_panel']
+        );
+    
+        return $tabs;
+    }
+
+    public function display_custom_product_tab_panel() {
+        echo '<h2>' . __('Custom Tab', 'woocommerce') . '</h2>';
+        echo '<p>' . __('Here\'s some custom content for your new tab.', 'woocommerce') . '</p>';
+    }
 }
-
-
 
 
