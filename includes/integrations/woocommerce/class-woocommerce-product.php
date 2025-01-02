cf8a333620211e919546666ffbf1349dab43e5d2
Taf Makura <tafmakura@gmail.com>
2025-01-02 20:04:22 +0200

    Update class-woocommerce-product.php

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -21,8 +21,9 @@ class Product {
         add_action('woocommerce_process_product_meta_variable', [$this, 'save_arsol_server_option_fields']);
         
         // Add custom tab
-        add_filter('woocommerce_product_tabs', [$this, 'add_custom_product_tab']);
-        add_action('woocommerce_product_tab_panels', [$this, 'display_custom_product_tab_panel']);
+        add_filter('woocommerce_product_data_tabs', [$this, 'add_custom_product_admin_tab']);
+        add_action('woocommerce_product_data_panels', [$this, 'add_custom_product_admin_tab_content']);
+        add_action('woocommerce_process_product_meta', [$this, 'save_custom_product_admin_tab_content']);
     }
 
     public function add_arsol_server_product_option($product_type_options) {
@@ -43,20 +44,32 @@ class Product {
         update_post_meta($post_id, '_arsol_server', $is_arsol_server);
     }
 
-    public function add_custom_product_tab($tabs) {
+    public function add_custom_product_admin_tab($tabs) {
         $tabs['custom_tab'] = array(
-            'title'    => __('Custom Tab', 'woocommerce'),
+            'label'    => __('Custom Tab', 'woocommerce'),
+            'target'   => 'custom_product_data',
+            'class'    => ['show_if_simple', 'show_if_variable'],
             'priority' => 50,
-            'callback' => [$this, 'display_custom_product_tab_panel']
         );
-    
+
         return $tabs;
     }
 
-    public function display_custom_product_tab_panel() {
-        echo '<h2>' . __('Custom Tab', 'woocommerce') . '</h2>';
-        echo '<p>' . __('Here\'s some custom content for your new tab.', 'woocommerce') . '</p>';
+    public function add_custom_product_admin_tab_content() {
+        echo '<div id="custom_product_data" class="panel woocommerce_options_panel">';
+        echo '<div class="options_group">';
+        woocommerce_wp_text_input(array(
+            'id'          => '_custom_field',
+            'label'       => __('Custom Field', 'woocommerce'),
+            'description' => __('Enter custom field data here.', 'woocommerce'),
+            'desc_tip'    => 'true',
+        ));
+        echo '</div>';
+        echo '</div>';
+    }
+
+    public function save_custom_product_admin_tab_content($post_id) {
+        $custom_field = isset($_POST['_custom_field']) ? sanitize_text_field($_POST['_custom_field']) : '';
+        update_post_meta($post_id, '_custom_field', $custom_field);
     }
 }
-
-
