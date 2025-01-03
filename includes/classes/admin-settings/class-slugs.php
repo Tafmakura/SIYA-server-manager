2ab712d1e546b07a71cb50688e63592a6b7f71ec
Taf Makura <tafmakura@gmail.com>
2025-01-03 23:55:02 +0200

    OOP

--- includes/classes/admin-settings/class-slugs.php
+++ includes/classes/admin-settings/class-slugs.php
@@ -6,7 +6,7 @@ class Slugs {
     /**
      * Display the slugs settings page
      */
-    public static function settings_page() {
+    public function settings_page() {
         require_once SIYA_PLUGIN_PATH . 'templates/admin/settings-page-slugs.php';
     }
 }
