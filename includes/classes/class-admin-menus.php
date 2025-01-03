d421b9e0c2d513342b119aa12c3224ed8d83269d
Taf Makura <tafmakura@gmail.com>
2025-01-04 00:12:27 +0200

    Update class-admin-menus.php

--- includes/classes/class-admin-menus.php
+++ includes/classes/class-admin-menus.php
@@ -14,13 +14,23 @@ class AdminMenus {
      */
     public function add_siya_menu() {
         add_menu_page(
-            'General',                         // Page title
-            'SIYA',                      // Menu title (changed)
+            'SIYA',                           // Page title
+            'SIYA',                           // Menu title
             'manage_options',                  // Capability
-            'siya',                            // Menu slug (changed)
+            'siya',                            // Menu slug
             array('Siya\AdminSettings\General', 'settings_page'), // Callback function
             'dashicons-admin-generic',         // Icon URL
             6                                  // Position
+        );
+
+        // Add General as first submenu
+        add_submenu_page(
+            'siya',                           // Parent slug
+            'General',                        // Page title
+            'General',                        // Menu title
+            'manage_options',                 // Capability
+            'siya',                          // Menu slug (same as parent to make it first item)
+            array('Siya\AdminSettings\General', 'settings_page') // Callback function
         );
     }
 
@@ -29,7 +39,7 @@ class AdminMenus {
      */
     public function add_slugs_submenu() {
         add_submenu_page(
-            'siya',                            // Parent slug (changed)
+            'siya',                            // Parent slug
             'Slugs',                           // Page title
             'Slugs',                           // Menu title
             'manage_options',                  // Capability
@@ -43,7 +53,7 @@ class AdminMenus {
      */
     public function add_api_submenu() {
         add_submenu_page(
-            'siya',                            // Parent slug (changed)
+            'siya',                            // Parent slug
             'API Keys',                         // Page title
             'API Keys',                         // Menu title
             'manage_options',                   // Capability
