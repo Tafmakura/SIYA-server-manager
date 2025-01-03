d2fd6d119a6394403acfa8d0264d35050a0d1d9e
Taf Makura <tafmakura@gmail.com>
2025-01-03 23:58:46 +0200

    Update class-admin-settings-slugs.php

--- includes/classes/class-admin-settings-slugs.php
+++ includes/classes/class-admin-settings-slugs.php
@@ -7,6 +7,6 @@ class Slugs {
      * Display the slugs settings page
      */
     public static function settings_page() {
-        require_once SIYA_PLUGIN_PATH . 'templates/admin/settings-page-slugs.php';
+        include plugin_dir_path(__DIR__) . '../templates/admin/settings-page-slugs.php';
     }
 }
