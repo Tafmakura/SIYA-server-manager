830e7777417934c896d25805b07c988c9abc2f57
Taf Makura <tafmakura@gmail.com>
2025-01-03 18:50:21 +0200

    Update settings-page.php

--- templates/admin/settings-page.php
+++ templates/admin/settings-page.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h1><?php _e('SIYA (Server Integration & Yield Augmentation)', 'arsol_siya'); ?></h1>
+    <h1><?php _e('SIYA API Keys)', 'arsol_siya'); ?></h1>
     <p><?php _e('Configure your API settings below. Ensure all fields are filled correctly.', 'arsol_siya'); ?></p>
     <form method="post" action="options.php">
         <?php
