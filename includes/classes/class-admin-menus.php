a1f40a35dd96988915494a30683f54f21d39e17c
Taf Makura <tafmakura@gmail.com>
2025-01-03 18:27:21 +0200

    Update class-admin-menus.php

--- includes/classes/class-admin-menus.php
+++ includes/classes/class-admin-menus.php
@@ -5,6 +5,7 @@ namespace Siya;
 class AdminMenus {
     public function __construct() {
         $this->add_siya_menu();
+        $this->add_api_submenu();
     }
 
     /**
@@ -19,6 +20,20 @@ class AdminMenus {
             array('Siya\AdminSettings\API', 'runcloud_settings_page'), // Callback function
             'dashicons-admin-generic',         // Icon URL
             6                                  // Position
+        );
+    }
+
+    /**
+     * Add API Settings submenu page
+     */
+    public function add_api_submenu() {
+        add_submenu_page(
+            'siya-menu',                       // Parent slug
+            'API Settings',                    // Page title
+            'API Settings',                    // Menu title
+            'manage_options',                  // Capability
+            'siya-api-settings',               // Menu slug
+            array('Siya\AdminSettings\API', 'runcloud_settings_page') // Callback function
         );
     }
 }
