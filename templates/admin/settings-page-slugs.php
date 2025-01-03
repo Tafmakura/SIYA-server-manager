ca80fb20b0a414bc8dbceeb861ffb8eda93eff64
Taf Makura <tafmakura@gmail.com>
2025-01-04 01:55:00 +0200

    Update settings-page-slugs.php

--- templates/admin/settings-page-slugs.php
+++ templates/admin/settings-page-slugs.php
@@ -52,10 +52,32 @@ if (!defined('ABSPATH')) {
                 </td>
             </tr>
             <tr>
-                <th scope="row">Provider Plans Slug</th>
+                <th scope="row">Plans</th>
                 <td>
-                    <input type="text" name="siya_digitalocean_plans_slug" 
-                           value="<?php echo esc_attr(get_option('siya_digitalocean_plans_slug', 'digitalocean-plans')); ?>" />
+                    <div class="plan-repeater" data-provider="digitalocean">
+                        <?php
+                        $digitalocean_plans = get_option('siya_digitalocean_plans', array());
+                        if (!empty($digitalocean_plans)) {
+                            foreach ($digitalocean_plans as $index => $plan) {
+                                ?>
+                                <div class="plan-row">
+                                    <input type="text" name="siya_digitalocean_plans[<?php echo $index; ?>][slug]" 
+                                           value="<?php echo esc_attr($plan['slug']); ?>" placeholder="Plan Slug" />
+                                    <textarea name="siya_digitalocean_plans[<?php echo $index; ?>][description]" 
+                                              maxlength="250" placeholder="Plan Description"><?php echo esc_textarea($plan['description']); ?></textarea>
+                                    <button type="button" class="button remove-plan">Remove</button>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <div class="plan-row template" style="display: none;">
+                            <input type="text" name="plan_slug[]" placeholder="Plan Slug" />
+                            <textarea name="plan_description[]" maxlength="250" placeholder="Plan Description"></textarea>
+                            <button type="button" class="button remove-plan">Remove</button>
+                        </div>
+                        <button type="button" class="button add-plan">Add Plan</button>
+                    </div>
                 </td>
             </tr>
         </table>
@@ -70,10 +92,32 @@ if (!defined('ABSPATH')) {
                 </td>
             </tr>
             <tr>
-                <th scope="row">Provider Plans Slug</th>
+                <th scope="row">Plans</th>
                 <td>
-                    <input type="text" name="siya_hetzner_plans_slug" 
-                           value="<?php echo esc_attr(get_option('siya_hetzner_plans_slug', 'hetzner-plans')); ?>" />
+                    <div class="plan-repeater" data-provider="hetzner">
+                        <?php
+                        $hetzner_plans = get_option('siya_hetzner_plans', array());
+                        if (!empty($hetzner_plans)) {
+                            foreach ($hetzner_plans as $index => $plan) {
+                                ?>
+                                <div class="plan-row">
+                                    <input type="text" name="siya_hetzner_plans[<?php echo $index; ?>][slug]" 
+                                           value="<?php echo esc_attr($plan['slug']); ?>" placeholder="Plan Slug" />
+                                    <textarea name="siya_hetzner_plans[<?php echo $index; ?>][description]" 
+                                              maxlength="250" placeholder="Plan Description"><?php echo esc_textarea($plan['description']); ?></textarea>
+                                    <button type="button" class="button remove-plan">Remove</button>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <div class="plan-row template" style="display: none;">
+                            <input type="text" name="plan_slug[]" placeholder="Plan Slug" />
+                            <textarea name="plan_description[]" maxlength="250" placeholder="Plan Description"></textarea>
+                            <button type="button" class="button remove-plan">Remove</button>
+                        </div>
+                        <button type="button" class="button add-plan">Add Plan</button>
+                    </div>
                 </td>
             </tr>
         </table>
@@ -88,10 +132,32 @@ if (!defined('ABSPATH')) {
                 </td>
             </tr>
             <tr>
-                <th scope="row">Provider Plans Slug</th>
+                <th scope="row">Plans</th>
                 <td>
-                    <input type="text" name="siya_vultr_plans_slug" 
-                           value="<?php echo esc_attr(get_option('siya_vultr_plans_slug', 'vultr-plans')); ?>" />
+                    <div class="plan-repeater" data-provider="vultr">
+                        <?php
+                        $vultr_plans = get_option('siya_vultr_plans', array());
+                        if (!empty($vultr_plans)) {
+                            foreach ($vultr_plans as $index => $plan) {
+                                ?>
+                                <div class="plan-row">
+                                    <input type="text" name="siya_vultr_plans[<?php echo $index; ?>][slug]" 
+                                           value="<?php echo esc_attr($plan['slug']); ?>" placeholder="Plan Slug" />
+                                    <textarea name="siya_vultr_plans[<?php echo $index; ?>][description]" 
+                                              maxlength="250" placeholder="Plan Description"><?php echo esc_textarea($plan['description']); ?></textarea>
+                                    <button type="button" class="button remove-plan">Remove</button>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <div class="plan-row template" style="display: none;">
+                            <input type="text" name="plan_slug[]" placeholder="Plan Slug" />
+                            <textarea name="plan_description[]" maxlength="250" placeholder="Plan Description"></textarea>
+                            <button type="button" class="button remove-plan">Remove</button>
+                        </div>
+                        <button type="button" class="button add-plan">Add Plan</button>
+                    </div>
                 </td>
             </tr>
         </table>
@@ -99,3 +165,52 @@ if (!defined('ABSPATH')) {
         <?php submit_button(); ?>
     </form>
 </div>
+
+<style>
+.plan-row {
+    margin-bottom: 10px;
+    padding: 10px;
+    background: #f9f9f9;
+    border: 1px solid #ddd;
+}
+
+.plan-row input[type="text"] {
+    width: 200px;
+    margin-right: 10px;
+}
+
+.plan-row textarea {
+    width: 300px;
+    height: 100px;
+    vertical-align: top;
+    margin-right: 10px;
+}
+
+.add-plan {
+    margin-top: 10px !important;
+}
+</style>
+
+<script>
+jQuery(document).ready(function($) {
+    $('.add-plan').on('click', function() {
+        var $repeater = $(this).closest('.plan-repeater');
+        var $template = $repeater.find('.template').clone();
+        var provider = $repeater.data('provider');
+        var index = $repeater.find('.plan-row').length - 1;
+
+        $template.removeClass('template').show()
+            .find('input, textarea').each(function() {
+                var name = $(this).attr('name');
+                $(this).attr('name', 'siya_' + provider + '_plans[' + index + '][' + 
+                    (name.includes('slug') ? 'slug' : 'description') + ']');
+            });
+
+        $(this).before($template);
+    });
+
+    $(document).on('click', '.remove-plan', function() {
+        $(this).closest('.plan-row').remove();
+    });
+});
+</script>
