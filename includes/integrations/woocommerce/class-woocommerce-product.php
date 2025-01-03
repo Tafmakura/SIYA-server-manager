4c334be295e7e6735c3450ea83259a52198d3b27
Taf Makura <tafmakura@gmail.com>
2025-01-03 03:21:54 +0200

    Update class-woocommerce-product.php

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -64,9 +64,11 @@ class Product {
     public function add_arsol_server_settings_tab_content() {
         echo '<div id="arsol_server_settings_data" class="panel woocommerce_options_panel">';
         echo '<div class="options_group">';
-        echo '<p class"arsol-notice" style="color: #856404; background-color: #fff3cd; padding: 10px; margin:10px; border: 1px solid #ffeeba; border-radius: 4px;">';
+        echo '<div class="inline notice woocommerce-message">';
+        echo '<p class="help">';
         echo __('Note: Changing server settings here will not affect servers associated with completed or pending subscriptions', 'woocommerce');
         echo '</p>';
+        echo '</div>';
         woocommerce_wp_text_input(array(
             'id'          => '_arsol_server_provider_slug',
             'label'       => __('Server Provider Slug', 'woocommerce'),
