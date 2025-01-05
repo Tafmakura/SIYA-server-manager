c8af95fc9caf45545f9e7d24f940b179e10aa360
Taf Makura <tafmakura@gmail.com>
2025-01-05 20:03:18 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -265,6 +265,11 @@ class ServerOrchestrator {
 
         // Ensure IPv4 address is available
         $ipv4 = get_post_meta($this->server_post_id, 'arsol_server_provisioned_ipv4', true);
+        if (is_null($ipv4)) {
+            error_log('[SIYA Server Manager] Error: IPv4 address is null.');
+            $subscription->add_order_note('RunCloud deployment failed: IPv4 address is null.');
+            return;
+        }
 
         // Deploy to RunCloud
         $runcloud_response = $this->runcloud->create_server_in_server_manager(
