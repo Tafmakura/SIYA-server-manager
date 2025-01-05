8d9af73f486e80e2a1a8192884eaa7dcaddf836c
Taf Makura <tafmakura@gmail.com>
2025-01-05 19:27:39 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -173,6 +173,7 @@ class ServerOrchestrator {
     // Get server post ID early since we need it for subsequent operations
         
         $server_provider_slug = get_post_meta($this->server_post_id, 'arsol_server_provider_slug', true);
+        $this->server_provider_slug = $server_provider_slug;
         error_log('[SIYA Server Manager] Server provider: ' . print_r($server_provider_slug, true));
         $server_name = 'ARSOL' . $this->subscription_id;
         $server_plan = get_post_meta($this->server_post_id, 'arsol_server_plan_slug', true);
