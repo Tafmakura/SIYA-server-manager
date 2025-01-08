08ba5b2f5aa817896b9793d14cbc81356d3fecba
Taf Makura <tafmakura@gmail.com>
2025-01-08 04:25:11 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -571,7 +571,7 @@ class ServerOrchestrator {
     }
 
     // Start server deletion process
-    public function start_server_deletion($post_id, $post) {
+    public function start_server_deletion($post_id) {
         error_log('[SIYA Server Manager - ServerOrchestrator] Milestone 1: Starting server deletion for post ID ' . $post_id);
         $post = get_post($post_id);
        if (!in_array($post->post_type, array('shop_subscription', 'woocommerce_subscription'))) {
