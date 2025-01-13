47e96c01a4a9abd2a2c90074cfcaa0329930246d
Taf Makura <tafmakura@gmail.com>
2025-01-13 19:27:55 +0200

    Update settings-page-ssh.php

--- templates/admin/settings-page-ssh.php
+++ templates/admin/settings-page-ssh.php
@@ -12,13 +12,13 @@
                 <td><input type="text" value="Server name (e.g. ARSOLXXXX)" style="min-width: 225px;" disabled /></td>
             </tr>
             <tr valign="top">
-                <th scope="row"><?php _e('Public Key', 'arsol_siya'); ?></th>
+                <th scope="row"><?php _e('SSH Public Key', 'arsol_siya'); ?></th>
                 <td>
                     <textarea name="arsol_global_ssh_public_key" rows="5" cols="50"><?php echo esc_textarea(get_option('arsol_global_ssh_public_key')); ?></textarea>
                 </td>
             </tr>
             <tr valign="top">
-                <th scope="row"><?php _e('Private Key', 'arsol_siya'); ?></th>
+                <th scope="row"><?php _e('SSH Private Key', 'arsol_siya'); ?></th>
                 <td>
                     <textarea name="arsol_global_ssh_private_key" rows="5" cols="50"><?php echo esc_textarea(get_option('arsol_global_ssh_private_key')); ?></textarea>
                 </td>
