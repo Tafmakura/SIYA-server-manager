09e0f77b698caca3137a06ea7af37fc4c94b05fd
Taf Makura <tafmakura@gmail.com>
2025-01-13 19:26:05 +0200

    xxx

--- includes/classes/class-admin-settings-ssh.php
+++ includes/classes/class-admin-settings-ssh.php
@@ -8,6 +8,11 @@ class SSH {
     }
 
     public static function settings_page() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            update_option('arsol_global_ssh_public_key', sanitize_textarea_field($_POST['arsol_global_ssh_public_key'] ?? ''));
+            update_option('arsol_global_ssh_private_key', sanitize_textarea_field($_POST['arsol_global_ssh_private_key'] ?? ''));
+        }
+
         include plugin_dir_path(__DIR__) . '../templates/admin/settings-page-ssh.php';
     }
 
--- templates/admin/settings-page-ssh.php
+++ templates/admin/settings-page-ssh.php
@@ -11,6 +11,18 @@
                 <th scope="row"><?php _e('SSH Username', 'arsol_siya'); ?></th>
                 <td><input type="text" value="Server name (e.g. ARSOLXXXX)" style="min-width: 225px;" disabled /></td>
             </tr>
+            <tr valign="top">
+                <th scope="row"><?php _e('Public Key', 'arsol_siya'); ?></th>
+                <td>
+                    <textarea name="arsol_global_ssh_public_key" rows="5" cols="50"><?php echo esc_textarea(get_option('arsol_global_ssh_public_key')); ?></textarea>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row"><?php _e('Private Key', 'arsol_siya'); ?></th>
+                <td>
+                    <textarea name="arsol_global_ssh_private_key" rows="5" cols="50"><?php echo esc_textarea(get_option('arsol_global_ssh_private_key')); ?></textarea>
+                </td>
+            </tr>
         </table>
         <?php submit_button(); ?>
     </form>
