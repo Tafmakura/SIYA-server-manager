19f7bbbfe45ca79069ec4860bb6b8cbc4a0f86f8
Taf Makura <tafmakura@gmail.com>
2025-01-14 00:53:03 +0200

    XXX

--- includes/classes/class-admin-settings-ssh.php
+++ includes/classes/class-admin-settings-ssh.php
@@ -24,7 +24,6 @@ class SSH {
         add_settings_field(
             'arsol_global_ssh_public_key',
             __('Public Key', 'arsol_siya'),
-         //   array($this, 'render_public_key_field'),
             'siya_settings_ssh',
             'siya_ssh_section'
         );
@@ -32,24 +31,10 @@ class SSH {
         add_settings_field(
             'arsol_global_ssh_private_key',
             __('Private Key', 'arsol_siya'),
-       //     array($this, 'render_private_key_field'),
             'siya_settings_ssh',
             'siya_ssh_section'
         );
     }
-
-   /*
-    public function render_public_key_field() {
-        $public_key = get_option('arsol_global_ssh_public_key', '');
-        echo "<textarea name='arsol_global_ssh_public_key' rows='5' cols='50'>" . esc_textarea($public_key) . "</textarea>";
-    }
-
-    public function render_private_key_field() {
-        $private_key = get_option('arsol_global_ssh_private_key', '');
-        echo "<textarea name='arsol_global_ssh_private_key' rows='5' cols='50'>" . esc_textarea($private_key) . "</textarea>";
-    }
-
-    */
 
     public static function settings_page() {
         include plugin_dir_path(__DIR__) . '../templates/admin/settings-page-ssh.php';
