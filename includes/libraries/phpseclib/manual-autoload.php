e611ecbe9d469d2bca052a57defbf59996040ea9
Taf Makura <tafmakura@gmail.com>
2025-01-14 04:22:03 +0200

    Create manual-autoload.php

---
+++ includes/libraries/phpseclib/manual-autoload.php
@@ -0,0 +1,7 @@
+<?php
+// Include the main SSH2 and RSA class files
+require_once plugin_dir_path(__DIR__) . '/libraries/phpseclib/Net/SSH2.php';
+require_once plugin_dir_path(__DIR__) . '/libraries/phpseclib/Crypt/RSA.php';
+
+// Add more class files here if needed
+?>
