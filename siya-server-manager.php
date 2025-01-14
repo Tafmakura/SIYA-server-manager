a2095fa38919061f0ec62782b518bf9e9bcc973f
Taf Makura <tafmakura@gmail.com>
2025-01-14 04:39:22 +0200

    dfg

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -9,23 +9,9 @@
 
 // Include the Setup class
 require_once plugin_dir_path(__FILE__) . 'includes/classes/class-setup.php';
-require_once plugin_dir_path(__FILE__) . 'includes/libraries/phpseclib/manual-autoload.php';
 
 use Siya\Setup;
 use phpseclib3\Net\SSH2;
 
 // Instantiate the Setup class
 $siyaServerManager = new Setup();
-
-$ssh_host = '137.184.156.94';
-$ssh_port = 22;
-
-$ssh = new SSH2($ssh_host, $ssh_port);
-if (!$ssh->login('username', 'password')) {
-    error_log('Failed to establish SSH connection to ' . $ssh_host . ' on port ' . $ssh_port);
-    echo 'Failed to establish SSH connection to ' . $ssh_host . ' on port ' . $ssh_port;
-} else {
-    error_log('SSH connection successful to ' . $ssh_host);
-    echo 'SSH connection successful to ' . $ssh_host;
-}
-
