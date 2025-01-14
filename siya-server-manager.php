b25c6f4e4bf23a5195ba71dfed1cd4d0c5e9c301
Taf Makura <tafmakura@gmail.com>
2025-01-14 05:21:13 +0200

    Update siya-server-manager.php

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -16,25 +16,4 @@ use Siya\Setup;
 // Instantiate the Setup class
 $siyaServerManager = new Setup();
 
-// Initialize the plugin
-
-echo 'HOYO>>>>>>>>>>>>>>>>>>>>>>>>';
-
-$ssh_host = 'staging.portal.automatedretail.io';
-$ssh_port = 22;
-
-if (function_exists('ssh2_connect')) {
-    try {
-        $ssh_connection = ssh2_connect($ssh_host, $ssh_port);
-        if ($ssh_connection) {
-            echo 'Connected to ' . $ssh_host . ' on port ' . $ssh_port . PHP_EOL;
-        } else {
-            echo 'Connection to ' . $ssh_host . ' on port ' . $ssh_port . ' failed.' . PHP_EOL;
-        }
-    } catch (Exception $e) {
-        echo 'An error occurred: ' . $e->getMessage() . PHP_EOL;
-    }
-} else {
-    echo 'The SSH2 PHP extension is not installed or enabled.' . PHP_EOL;
-}
 
