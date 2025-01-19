cf66614c25e1f185609a188e3fe0105d4d7f8210
Taf Makura <tafmakura@gmail.com>
2025-01-20 00:59:03 +0200

    Update siya-server-manager.php

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -25,13 +25,20 @@ require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
 
 
-add_filter('wcs_view_subscription_columns', 'add_custom_hello_text_to_status_column', 10, 2);
+add_filter('manage_edit-wcs_subscription_columns', 'add_custom_status_column');
+add_action('manage_wcs_subscription_posts_custom_column', 'render_custom_status_column', 10, 2);
 
-function add_custom_hello_text_to_status_column($column, $subscription) {
-    if ($column == 'status') {
-        $status = wcs_get_subscription_status($subscription);
+function add_custom_status_column($columns) {
+    $columns['subscription_status'] = __('Subscription Status', 'text_domain');
+    return $columns;
+}
+
+function render_custom_status_column($column, $post_id) {
+    if ($column == 'subscription_status') {
+        $subscription = wcs_get_subscription($post_id);
+        $status = $subscription->get_status();
         $status_text = wcs_get_subscription_status_name($status);
-        echo '<span>Hello, here is the status:</span><br>' . $status_text;
+        echo '<span>Hello, here is the status:</span> ' . $status_text;
     }
 }
 
