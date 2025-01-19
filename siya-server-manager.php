bd1192753896bc803855f03293371490a84a357c
Taf Makura <tafmakura@gmail.com>
2025-01-20 00:41:24 +0200

    Update siya-server-manager.php

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -25,23 +25,22 @@ require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 // Custom Subscription Status Handler
 class SIYA_Subscription_Status {
     public function __construct() {
-        // Register the custom status after WC Subscriptions is loaded
-        add_action('woocommerce_loaded', array($this, 'register_custom_post_status'));
-        
-        // Add status to lists
-        add_filter('wcs_order_statuses', array($this, 'add_custom_post_status'));
-        add_filter('woocommerce_reports_order_statuses', array($this, 'add_custom_post_status'));
-        add_filter('wc_order_statuses', array($this, 'add_custom_post_status'));
-        
-        // Add custom status bulk actions
-        add_action('admin_footer', array($this, 'custom_status_bulk_actions'), 100);
+        add_action('init', array($this, 'register_status'));
+        add_filter('wc_order_statuses', array($this, 'add_status_to_order'));
+        add_filter('wcs_subscription_statuses', array($this, 'add_status_to_subscription'));
+        add_filter('woocommerce_subscription_statuses', array($this, 'add_status_to_subscription'));
+        add_filter('woocommerce_subscriptions_renewal_statuses', array($this, 'add_status_to_subscription'));
+        add_filter('woocommerce_composite_subscriptions_statuses', array($this, 'add_status_to_subscription'));
+        add_filter('woocommerce_admin_order_actions', array($this, 'add_subscription_actions'), 10, 2);
     }
 
-    public function register_custom_post_status() {
-        global $wc_current_version;
-
+    public function register_status() {
+        if(!get_term_by('slug', 'custom-status', 'shop_subscription_status')) {
+            wp_insert_term('Custom Status', 'shop_subscription_status', array('slug' => 'custom-status'));
+        }
+        
         register_post_status('wc-custom-status', array(
-            'label' => _x('Custom Status', 'Order status', 'arsol'),
+            'label' => __('Custom Status', 'arsol'),
             'public' => true,
             'show_in_admin_status_list' => true,
             'show_in_admin_all_list' => true,
@@ -52,27 +51,40 @@ class SIYA_Subscription_Status {
         ));
     }
 
-    public function add_custom_post_status($order_statuses) {
+    public function add_status_to_subscription($statuses) {
+        $new_statuses = array();
+        // Add status after active
+        foreach ($statuses as $key => $status) {
+            $new_statuses[$key] = $status;
+            if ($key === 'wc-active') {
+                $new_statuses['wc-custom-status'] = _x('Custom Status', 'Subscription status', 'arsol');
+            }
+        }
+        return $new_statuses;
+    }
+
+    public function add_status_to_order($order_statuses) {
         $order_statuses['wc-custom-status'] = _x('Custom Status', 'Order status', 'arsol');
         return $order_statuses;
     }
 
-    public function custom_status_bulk_actions() {
-        global $post_type;
-        if ($post_type == 'shop_subscription') {
-            ?>
-            <script type="text/javascript">
-                jQuery(function() {
-                    jQuery('<option>').val('mark_custom-status').text('<?php _e('Change status to Custom Status', 'arsol')?>').appendTo("select[name='action']");
-                    jQuery('<option>').val('mark_custom-status').text('<?php _e('Change status to Custom Status', 'arsol')?>').appendTo("select[name='action2']");
-                });
-            </script>
-            <?php
+    public function add_subscription_actions($actions, $subscription) {
+        if ($subscription->get_type() === 'shop_subscription') {
+            if ($subscription->get_status() !== 'custom-status') {
+                $actions['custom_status'] = array(
+                    'url' => wp_nonce_url(admin_url('admin-ajax.php?action=woocommerce_subscription_mark_custom-status&subscription_id=' . $subscription->get_id())),
+                    'name' => __('Custom Status', 'arsol'),
+                    'action' => 'custom-status'
+                );
+            }
         }
+        return $actions;
     }
 }
 
-// Initialize immediately rather than with plugins_loaded
-$siya_subscription_status = new SIYA_Subscription_Status();
+// Initialize the status handler
+add_action('woocommerce_init', function() {
+    new SIYA_Subscription_Status();
+});
 
 
