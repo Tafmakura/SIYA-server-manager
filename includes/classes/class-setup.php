6990ddcd97d78e207379fe2dacffa3abfd13afb7
Taf Makura <tafmakura@gmail.com>
2025-01-20 00:06:21 +0200

    Circuit breaker

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -43,7 +43,7 @@ class Setup {
         require_once plugin_dir_path(__DIR__) . '/integrations/server-providers/vultr/class-vultr.php';
  
         require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-orchestrator.php';
-      // DELETE  require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-circuit-breaker.php';
+        require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-circuit-breaker.php';
         require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce/class-woocommerce-product.php';
 
         // require_once plugin_dir_path(__DIR__) . '/libraries/phpseclib/autoload.php';
