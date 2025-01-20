36470f57adb3e69fdaebcd67c76a2ec44c0ee226
Taf Makura <tafmakura@gmail.com>
2025-01-20 04:33:16 +0200

    Update siya-server-manager.php

--- siya-server-manager.php
+++ siya-server-manager.php
@@ -19,30 +19,3 @@ $siyaServerManager = new Setup();
 
 // Include the Composer autoload to load phpseclib classes
 require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
-
-
-
-
-
-
-add_filter('manage_edit-wcs_subscription_columns', 'add_custom_status_column');
-add_action('manage_wcs_subscription_posts_custom_column', 'render_custom_status_column', 10, 2);
-
-function add_custom_status_column($columns) {
-    $columns['subscription_status'] = __('Subscription Status', 'text_domain');
-    return $columns;
-}
-
-function render_custom_status_column($column, $post_id) {
-    if ($column == 'subscription_status') {
-        $subscription = wcs_get_subscription($post_id);
-        $status = $subscription->get_status();
-        $status_text = wcs_get_subscription_status_name($status);
-        echo '<span>Hello, here is the status:</span> ' . $status_text;
-    }
-}
-
-
-
-
-
