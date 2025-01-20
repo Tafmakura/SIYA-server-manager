7d383b80a2662fcb615ab57ceb04839ddee4b3af
Taf Makura <tafmakura@gmail.com>
2025-01-20 10:08:18 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -1019,7 +1019,7 @@ class ServerOrchestrator {
 
         // Check circuit breaker status for power-up
         $this->server_circuit_breaker_position = get_post_meta($server_post_id, '_arsol_state_00_circuit_breaker', true);
-        if ($this->server_circuit_breaker_position == -1 || $this->server_circuit_breaker_position == 1) {
+        if ($this->server_circuit_breaker_position == -1) {
             error_log('#042 [SIYA Server Manager - ServerOrchestrator] Server circuit breaker for subscription ' . $subscription_id . ' is tripped or in progress. Skipping power-up.');
             return;
         }
