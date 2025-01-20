659e394ae059c3dfe91c52a62507da0ae4ce2ff0
Taf Makura <tafmakura@gmail.com>
2025-01-20 22:22:48 +0200

    Update class-setup.php

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -44,7 +44,7 @@ class Setup {
  
         require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-orchestrator.php';
         require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/class-server-circuit-breaker.php';
-        require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/statuses/class-server-error.php';
+      //  require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce-subscriptions/statuses/class-server-error.php';
 
 
         require_once plugin_dir_path(__DIR__) . '/integrations/woocommerce/class-woocommerce-product.php';
@@ -72,9 +72,9 @@ class Setup {
         if (class_exists('Siya\Integrations\WooCommerceSubscriptions\ServerOrchestrator')) {
             $orchestrator = new \Siya\Integrations\WooCommerceSubscriptions\ServerOrchestrator();
         }
-        if (class_exists('Siya\Integrations\WooCommerceSubscriptions\Statuses\ServerError')) {
-            $statuses = new \Siya\Integrations\WooCommerceSubscriptions\Statuses\ServerError();
-        }
+      //  if (class_exists('Siya\Integrations\WooCommerceSubscriptions\Statuses\ServerError')) {
+     //       $statuses = new \Siya\Integrations\WooCommerceSubscriptions\Statuses\ServerError();
+     //   }
         if (class_exists(' Siya\Integrations\WooCommerce\Product')) {
             $woocommerce_product = new \Siya\Integrations\WooCommerce\Product();
         }
