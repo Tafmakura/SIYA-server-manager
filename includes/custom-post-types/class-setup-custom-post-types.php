10a88cdef24c303babca0ad70c51fe992ffdc1f6
Taf Makura <tafmakura@gmail.com>
2025-01-21 09:25:15 +0200

    Update class-setup-custom-post-types.php

--- includes/custom-post-types/class-setup-custom-post-types.php
+++ includes/custom-post-types/class-setup-custom-post-types.php
@@ -12,7 +12,9 @@ class CustomPostTypes {
 
     private function require_files() {
         // Custom Post Types
+        require_once plugin_dir_path(__FILE__) . 'server/class-server-cpt-setup.php';
         require_once plugin_dir_path(__FILE__) . 'server/class-server-cpt.php';
+
     }
 
     private function instatiate_classes() {
