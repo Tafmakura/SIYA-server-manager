fe90580001550e21f769a9b738b10d802fda1aef
Taf Makura <tafmakura@gmail.com>
2025-01-22 07:53:17 +0200

    Update settings-page-general.php

--- templates/admin/settings-page-general.php
+++ templates/admin/settings-page-general.php
@@ -6,4 +6,17 @@
         <h2>Quick Overview</h2>
         <p>Configure your RunCloud API settings in the API Settings submenu to get started.</p>
     </div>
+
+    <form method="post" action="options.php">
+        <?php settings_fields('siya_settings_general'); ?>
+        <label for="arsol_allow_admin_server_delition">
+            Allow server delition by admin
+        </label>
+        <input
+            type="checkbox"
+            name="arsol_allow_admin_server_delition"
+            value="1"
+            <?php checked(get_option('arsol_allow_admin_server_delition'), 1); ?>
+        />
+    </form>
 </div>
