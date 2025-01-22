850c725438090576bf22d8c77bd486d839aa996a
Taf Makura <tafmakura@gmail.com>
2025-01-22 12:13:50 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -1527,13 +1527,11 @@ class ServerOrchestrator {
 
             error_log('[SIYA Server Manager] HOOOOOOO0OOOOOOOOOOOOOOOOOOOOOOOOOOOO Server product metadata: ' . print_r($red, true));
 
-
+         
             $meta_values = get_post_meta($this->server_product_id, '_arsol_assigned_server_groups', false);
-        
-            foreach ($meta_values as $meta_value) {
-                error_log('Meta value: ' . print_r($meta_value, true));
-            }
-
+            
+            error_log(print_r($meta_values));
+            
 
 
 
