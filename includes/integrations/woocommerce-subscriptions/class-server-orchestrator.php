ed784465d6ddc5a2286d393975a5bb03cffda0fc
Taf Makura <tafmakura@gmail.com>
2025-01-24 23:01:12 +0200

    Update class-server-orchestrator.php

--- includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
+++ includes/integrations/woocommerce-subscriptions/class-server-orchestrator.php
@@ -309,7 +309,11 @@ class ServerOrchestrator {
             }
 
             // Check latest server post status 
+            // Get server metadata to check current status
+            $metadata = $server_post_instance->get_meta_data($this->server_post_id);
+            $this->server_post_status = $metadata['_arsol_state_05_server_post'] ?? null;
 
+            error_log('Checking server post status: ' . $this->server_post_status);
 
             if ($this->server_post_status == 2) {
 
