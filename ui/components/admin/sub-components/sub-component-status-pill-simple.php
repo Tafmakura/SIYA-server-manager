a04f75625f07d7db80d5476a5422ec7ca861616c
Taf Makura <tafmakura@gmail.com>
2025-01-25 21:50:41 +0200

    Server status

--- ui/components/admin/sub-components/sub-component-status-pill-simple.php
+++ ui/components/admin/sub-components/sub-component-status-pill-simple.php
@@ -46,8 +46,8 @@ function arsol_sub_component_status_pill_simple($server_post_id){
     <style>
         .server-status.available { background: #c6e1c6; color: #5b841b; }
         .server-status.building { background: #f8dda7; color: #94660c; }
-        .server-status.error { background: #eba3a3; color: #761919; }
-        .server-status.repairing { background: #c8d7e1; color: #194d70; }
+        .server-status.error, .server-status.repairing { background: #eba3a3; color: #761919; }
+        /*.server-status.repairing { background: #c8d7e1; color: #194d70; }*/
         .server-status.no-server { background: #e5e5e5; color: #777; }
     </style>
 <?php }
--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -8,7 +8,7 @@ function arsol_sub_component_status_subscription_page($server_post_id){
     <p class="form-field form-field-wide arsol-server-status" style="margin-top: 1em;" ><p>
     <h3>Related Server</h3>
     <p class="form-field form-field-wide arsol-server-status-pill" >
-    <p><strong>Important: </strong><?php echo esc_html__("A Server state represents the provisioning status of a server and not its current power state.", 'arsol-server-manager'); ?></p>
+    <p><strong>Important: </strong><?php echo esc_html__("A Server state represents the provisioning state of a server and not its current power state.", 'arsol-server-manager'); ?></p>
     <p class="form-field form-field-wide">Server status: <a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?> →</a></p>
     <p class="form-field form-field-wide arsol-server-status-pill" style="margin-top: 10px;">
         <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
