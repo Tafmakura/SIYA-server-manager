3a5bfd7b4df31f9a08eb3e08e2f3330a4d19e2ff
Taf Makura <tafmakura@gmail.com>
2025-01-25 11:15:54 +0200

    Update sub-component-status-subscription-page.php

--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -5,15 +5,24 @@ function arsol_sub_component_status_subscription_page($server_post_id){
     $subscription_id = get_post_meta($server_post_id, 'arsol_server_subscription_id', true);
     $subscription = wcs_get_subscription($subscription_id);?>
 
-    <h3>Server status</h3>
-    <p class="form-field form-field-wide">
-        <div class="arsol-server-status" style="margin-top: 10px;">
+    <div class="arsol-server-status">
+        <h3>Server status</h3>
+        <div class="arsol-server-status-pill" style="margin-top: 10px;">
             <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
         </div>
         <p class="description">Server name: <a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?></a></p>
-    </p>
+    </div>
     <style>
-        
+        .arsol-server-status {
+            margin-top: 1em;
+        }
+        .arsol-server-status-pill {
+            display: inline-block;
+        }
+        .arsol-server-status-pill {
+            margin-top: 9px;
+        }
+
     </style>
 
 <?php }
