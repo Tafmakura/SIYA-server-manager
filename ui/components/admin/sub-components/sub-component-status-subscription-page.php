483fed31a4498e22ea0351113b98777e4240f187
Taf Makura <tafmakura@gmail.com>
2025-01-25 11:57:23 +0200

    Update sub-component-status-subscription-page.php

--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -6,16 +6,14 @@ function arsol_sub_component_status_subscription_page($server_post_id){
     $subscription = wcs_get_subscription($subscription_id);?>
 
   
-    <p class="form-field form-field-wide arsol-server-status" ><p>
+    <p class="form-field form-field-wide arsol-server-status" style="margin-top: 1em;" ><p>
     <h3>Server status</h3>
-    <p class="form-field form-field-wide arsol-server-status-pill" style="margin-top: 10px;">
-    <p><?php esc_html_e('An active status indicates the server was installed correctly and remains available, even when powered off. Servers in error state indicate installation issues and may be completely unavailable or operating with limited functionality.', 'arsol-server-manager'); ?></p>
-        <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
-    <p>Server statuses indicate the current state of your server. Possible states are: 
-    <strong>Active</strong> (server is running), 
-    <strong>Inactive</strong> (server is stopped), 
-    <strong>Suspended</strong> (subscription paused), or 
-    <strong>Error</strong> (requires attention).</p>
+    <p class="form-field form-field-wide arsol-server-status-pill" >
+    <p>Possible server states states are: 
+    <strong>Active</strong> (server installed correctly), 
+    <strong>Building</strong> (server is setting up for the first time), 
+    <strong>Repairing</strong> (server is attempting an auto-repair procesdure),
+    <strong>Error</strong> (Server did not install correctly, requires attention).</p>
     <p class="form-field form-field-wide arsol-server-status-pill" style="margin-top: 10px;">
         <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
     </p>
