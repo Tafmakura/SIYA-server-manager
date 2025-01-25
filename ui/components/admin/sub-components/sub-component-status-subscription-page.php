84fe50696375cda323e2385cc7b563dd7dfe1972
Taf Makura <tafmakura@gmail.com>
2025-01-25 21:26:51 +0200

    Update sub-component-status-subscription-page.php

--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -8,8 +8,8 @@ function arsol_sub_component_status_subscription_page($server_post_id){
     <p class="form-field form-field-wide arsol-server-status" style="margin-top: 1em;" ><p>
     <h3>Server</h3>
     <p class="form-field form-field-wide arsol-server-status-pill" >
-    <p><b><?php echo esc_html__("Important:", 'arsol-server-manager'); ?></b> <?php echo esc_html__("A Server status represents the provision status of the server and not its current power state.", 'arsol-server-manager'); ?></p>
-    <p class="form-field form-field-wide"><b>Server name: </b><a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?> →</a></p>
+    <p><b><?php echo esc_html__("A Server status represents the provisioning status of a server and not its current power state.", 'arsol-server-manager'); ?></p>
+    <p class="form-field form-field-wide"><b>Server status: </b><a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?> →</a></p>
     <p class="form-field form-field-wide arsol-server-status-pill" style="margin-top: 10px;">
         <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
     </p>
