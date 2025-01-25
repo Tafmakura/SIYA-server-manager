a46cb2b2cc4ea6d98d9a14f16819b006509b1770
Taf Makura <tafmakura@gmail.com>
2025-01-25 04:20:43 +0200

    Update class-server-error.php

--- includes/integrations/woocommerce-subscriptions/statuses/class-server-error.php
+++ includes/integrations/woocommerce-subscriptions/statuses/class-server-error.php
@@ -25,7 +25,7 @@ class ServerError {
             if ('status' === $key) {
                 $new_columns['arsol-server-status'] = __('Server', 'siya-text-domain');
                 // Add width style
-                echo '<style>.column-arsol-server-status { width: 80px; }</style>';
+                echo '<style>.column-arsol-server-status { width: 90px; }</style>';
             }
         }
         return $new_columns;
