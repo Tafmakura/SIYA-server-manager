5b0c5381a081f0c87524cde3cdf34f5edb877055
Taf Makura <tafmakura@gmail.com>
2025-01-25 10:57:19 +0200

    Update sub-component-status-subscription-page.php

--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -5,7 +5,7 @@ function arsol_sub_component_status_subscription_page($server_post_id){
     $subscription_id = get_post_meta($server_post_id, 'arsol_server_subscription_id', true);
     $subscription = wcs_get_subscription($subscription_id);?>
 
-    <h3>Server</h3>
+    <h3>Server status</h3>
     <p class="form-field form-field-wide">
         <div class="arsol-server-status" style="margin-top: 10px;">
             <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
