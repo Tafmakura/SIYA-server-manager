2c369e16321acfe744600e5c9578478d8bf14538
Taf Makura <tafmakura@gmail.com>
2025-01-25 10:54:44 +0200

    Update sub-component-status-subscription-page.php

--- ui/components/admin/sub-components/sub-component-status-subscription-page.php
+++ ui/components/admin/sub-components/sub-component-status-subscription-page.php
@@ -7,10 +7,10 @@ function arsol_sub_component_status_subscription_page($server_post_id){
 
     <h3>Server</h3>
     <p class="form-field form-field-wide">
-        <p class="description">Server name: <a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?></a></p>
         <div class="arsol-server-status" style="margin-top: 10px;">
             <?php arsol_sub_component_status_pill_simple($server_post_id); ?>
         </div>
+        <p class="description">Server name: <a href="<?php echo esc_url(get_edit_post_link($server_post_id)); ?>">ARSOL<?php echo $subscription_id; ?></a></p>
     </p>
     <style>
         
