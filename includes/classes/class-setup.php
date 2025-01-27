7620c56800a144c8d24f41ec32e5a16cc3f669f2
Taf Makura <tafmakura@gmail.com>
2025-01-27 13:20:23 +0200

    Update class-setup.php

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -37,7 +37,7 @@ class Setup {
        
         
         // Integrations
-        require_once plugin_dir_path(__DIR__) . '/integrations/class-integrations-setup.php';
+        require_once __SIYA_PLUGIN_ROOT__ . '/includes/integrations/class-integrations-setup.php';
    
         // UI
         require_once __SIYA_PLUGIN_ROOT__ . '/ui/components/class-components-setup.php';
