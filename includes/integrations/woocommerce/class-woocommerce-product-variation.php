7e5fbd9145c7f8dc1465a637b039f0dc5fde179b
Taf Makura <tafmakura@gmail.com>
2025-01-27 16:27:08 +0200

    Update class-woocommerce-product-variation.php

--- includes/integrations/woocommerce/class-woocommerce-product-variation.php
+++ includes/integrations/woocommerce/class-woocommerce-product-variation.php
@@ -2,11 +2,11 @@
 
 namespace Siya\Integrations\WooCommerce\Product;
 
-use Siya\AdminSettings\Slugs;
+use  Siya\Integrations\WooCommerce\Product;
 
 defined('ABSPATH') || exit;
 
-class Variation {
+class Variation extends Product {
    
     public function __construct() {
 
