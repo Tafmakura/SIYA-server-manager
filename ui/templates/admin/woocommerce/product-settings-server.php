2582a3bf5b7ead5cb97e94f4a984476ef1cca971
Taf Makura <tafmakura@gmail.com>
2025-01-27 06:50:16 +0200

    Update product-settings-server.php

--- ui/templates/admin/woocommerce/product-settings-server.php
+++ ui/templates/admin/woocommerce/product-settings-server.php
@@ -464,11 +464,9 @@ jQuery(document).ready(function($) {
     function toggle_arsol_server_settings_tab() {
         if ($('#_arsol_server').is(':checked')) {
             $('#woocommerce-product-data .arsol_server_settings_options').show();
-            $('#_virtual').prop('checked', true); // Check _virtual
         } else {
             $('#woocommerce-product-data .arsol_server_settings_options').hide();
             $('.wc-tabs .general_tab a').click();
-            $('#_virtual').prop('checked', false); // Uncheck _virtual
         }
     }
 
