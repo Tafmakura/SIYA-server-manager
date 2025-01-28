67361b9eb417edb4337131b8b6d82c2a71310e90
Taf Makura <tafmakura@gmail.com>
2025-01-28 05:52:40 +0200

    update

--- includes/integrations/woocommerce/class-woocommerce-product.php
+++ includes/integrations/woocommerce/class-woocommerce-product.php
@@ -57,7 +57,7 @@ class Product {
         $tabs['arsol_server_settings'] = array(
             'label'    => __('Server Settings', 'woocommerce'),
             'target'   => 'arsol_server_settings_data',
-            'class'    => array('show_if_arsol_server'), // This matches WooCommerce's pattern
+            'class'    => array('show_if_subscription', 'show_if_variable-subscription', 'show_if_arsol_server'),
             'priority' => 50,
         );
 
