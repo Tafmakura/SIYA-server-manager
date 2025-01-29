6703c008d750458a4d9a8b351d794dbc933ed1a0
Taf Makura <tafmakura@gmail.com>
2025-01-29 17:47:26 +0200

    Update product-settings-server.php

--- ui/templates/admin/woocommerce/product-settings-server.php
+++ ui/templates/admin/woocommerce/product-settings-server.php
@@ -510,11 +510,13 @@ jQuery(document).ready(function($) {
 
     $('#arsol_server_type').on('change', function() {
         var serverType = $(this).val();
-        updateServerTypeFields(serverType);
+        // First run visibility toggles
         toggleArsolSitesServerElements();
-        toggleArsolApplicationServerElements(); // Add this line
-        toggleArsolHideSitesServerElements(); // Add this line
-        toggleArsolHideApplicationServerElements(); // Add this line
+        toggleArsolApplicationServerElements();
+        toggleArsolHideSitesServerElements();
+        toggleArsolHideApplicationServerElements();
+        // Then run other updates that might override
+        updateServerTypeFields(serverType);
     });
 
     // Initial load
@@ -564,11 +566,25 @@ jQuery(document).ready(function($) {
         var $elements = $('.show_if_arsol_sites_server');
         var serverType = $('#arsol_server_type').val();
         
+        console.log('Sites Server Toggle:', {
+            'Server Type': serverType,
+            'Elements Found': $elements.length,
+            'Current Visibility': $elements.is(':visible'),
+            'Has Hidden Class': $elements.hasClass('hidden'),
+            'Current Style': $elements.attr('style')
+        });
+        
         if (serverType === 'sites_server') {
             $elements.attr('style', '').removeClass('hidden');
         } else {
             $elements.attr('style', 'display: none !important').addClass('hidden');
         }
+        
+        console.log('After Toggle:', {
+            'Visibility': $elements.is(':visible'),
+            'Has Hidden Class': $elements.hasClass('hidden'),
+            'Style': $elements.attr('style')
+        });
     }
 
     // Handle application server element visibility
