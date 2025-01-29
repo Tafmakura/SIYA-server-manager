2b40df59c20f39694656889c74a09c494ee9c206
Taf Makura <tafmakura@gmail.com>
2025-01-29 04:08:44 +0200

    Update class-woocommerce-product-variation.php

--- includes/integrations/woocommerce/class-woocommerce-product-variation.php
+++ includes/integrations/woocommerce/class-woocommerce-product-variation.php
@@ -93,10 +93,11 @@ class Variation {
                     $has_errors = true;
                 }
                 if (!preg_match('/^[a-zA-Z0-9-]+$/', $image)) {
-                    \WC_Admin_Settings::add_error(__('Invalid server image. Only letters, numbers, and hyphens allowed.', 'woocommerce'));
-                    wc_add_notice(__('Invalid server image. Only letters, numbers, and hyphens allowed.', 'woocommerce'), 'error');
-                    $has_errors = true;
-                    return;
+                wc_add_notice(__('Invalid server image. Only letters, numbers, and hyphens allowed.', 'woocommerce'), 'error');
+                   
+                echo "Invalid server image. Only letters, numbers, and hyphens allowed.";
+                
+                $has_errors = true;
                 }
             }
 
