aa4079ad4d497c29f8072cf41fde01173f641831
Taf Makura <tafmakura@gmail.com>
2025-01-30 09:11:57 +0200

    Update product-settings-server.php

--- ui/templates/admin/woocommerce/product-settings-server.php
+++ ui/templates/admin/woocommerce/product-settings-server.php
@@ -500,10 +500,10 @@ jQuery(document).ready(function($) {
     $('#arsol_server').on('change', function() {
         if (!$(this).is(':checked')) {
             // Check if we're currently on the server settings tab
-            if ($('.arsol_server_settings_tab').hasClass('active')) {
+            if ($('.arsol_server_settings_tab a').hasClass('active')) {
                 // Try to find and click general tab first, then variations tab as fallback
-                const $generalTab = $('.general_tab a');
-                const $variationsTab = $('.variations_tab a');
+                const $generalTab = $('.general_tab');
+                const $variationsTab = $('.variations_tab');
                 
                 if ($generalTab.length) {
                     $generalTab[0].click();
