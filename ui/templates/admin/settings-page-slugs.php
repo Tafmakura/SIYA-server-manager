2b83b495770a6f5830f3067a93e0d7f8665254d3
Taf Makura <tafmakura@gmail.com>
2025-01-31 15:53:54 +0200

    Update settings-page-slugs.php

--- ui/templates/admin/settings-page-slugs.php
+++ ui/templates/admin/settings-page-slugs.php
@@ -165,10 +165,13 @@ function render_repeater_section($provider_slug, $plans) {
                         <select name="siya_wp_server_group" id="siya_wp_server_group">
                             <?php
                             $provider = get_option('siya_wp_server_provider');
-                            $groups = (new Siya\AdminSettings\Slugs())->get_provider_group_slugs($provider);
+                            $slugs = new Siya\AdminSettings\Slugs();
+                            $groups = $slugs->get_provider_plan_group_slugs_by_server_type($provider, 'sites_server');
                             $selected_group = get_option('siya_wp_server_group');
                             foreach ($groups as $group) {
-                                echo '<option value="' . esc_attr($group) . '" ' . selected($selected_group, $group, false) . '>' . esc_html($group) . '</option>';
+                                echo '<option value="' . esc_attr($group) . '" ' . 
+                                    selected($selected_group, $group, false) . '>' . 
+                                    esc_html($group) . '</option>';
                             }
                             ?>
                         </select>
