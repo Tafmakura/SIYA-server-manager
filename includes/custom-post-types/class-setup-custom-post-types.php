921d945fd6be3556cd71c85527a8d553ae7e2a56
Taf Makura <tafmakura@gmail.com>
2025-02-02 13:46:12 +0200

    blue prints

--- includes/custom-post-types/class-setup-custom-post-types.php
+++ includes/custom-post-types/class-setup-custom-post-types.php
@@ -6,7 +6,7 @@ class CustomPostTypes {
     public function __construct() {
        
         $this->require_files();
-        $this->instatiate_classes();
+        $this->instantiate_classes();
 
     }
 
@@ -14,14 +14,12 @@ class CustomPostTypes {
         // Custom Post Types
         require_once plugin_dir_path(__FILE__) . 'server/class-server-cpt-setup.php';
         require_once plugin_dir_path(__FILE__) . 'application/class-application-cpt-setup.php';
-
+        require_once plugin_dir_path(__FILE__) . 'blueprint/class-blueprint-cpt-setup.php';
     }
 
-    private function instatiate_classes() {
+    private function instantiate_classes() {
         new \Siya\CustomPostTypes\ServerPost\Setup();
         new \Siya\CustomPostTypes\ApplicationPost\Setup();
+        new \Siya\CustomPostTypes\BlueprintPost\Setup();
     }
-
-
-    
 }
